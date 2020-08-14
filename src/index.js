bfd3eccb7bce5bbbc9eeb31c07cb548572802ef6
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2020-08-13 22:51:31 -0600

    Troubleshoot deployment

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import thunk from "redux-thunk";
 import { reducer } from "./reducers";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+require("dotenv").config();
 
 ReactDOM.render(
   <Provider store={createStore(reducer, applyMiddleware(thunk))}>
